d966a4a842ea7ecd7cd2d15b3a58002ea26d5348
rabrennie <rennierab@gmail.com>
2019-08-23 22:51:32 +0100

    Add ServerlessDeployCommand

---
+++ src/Console/Commands/ServerlessDeployCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace LaravelServerless\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ServerlessDeployCommand extends Command
+{
+
+    protected $signature = 'serverless:deploy';
+    protected $description = 'Deploys service to remote serverless service.';
+
+    public function handle()
+    {
+        // TODO: write command
+    }
+}
